e8b0f13280902fc8095b8205c130ff51bb93c1b1
teo <ramones.teo@gmail.com>
2017-03-23 14:28:21 +0200

    Adds fragments

---
+++ app/src/main/java/draw/statusbar/drawstatusbar/FirstFragment.java
@@ -0,0 +1,32 @@
+package draw.statusbar.drawstatusbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by css on 3/23/17.
+ */
+
+public class FirstFragment extends Fragment {
+
+    public static FirstFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        FirstFragment fragment = new FirstFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/draw/statusbar/drawstatusbar/SecondFragment.java
@@ -0,0 +1,33 @@
+package draw.statusbar.drawstatusbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by css on 3/23/17.
+ */
+
+public class SecondFragment extends Fragment{
+
+    public static SecondFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        SecondFragment fragment = new SecondFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        return view;
+    }
+
+}
