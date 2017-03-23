1f8a038a7c9116d614e487d565e0d9bfe83e3a3a
teo <ramones.teo@gmail.com>
2017-03-23 16:38:28 +0200

    Adds and removes translucent status bar

--- app/src/main/java/draw/statusbar/drawstatusbar/MainActivity.java
+++ app/src/main/java/draw/statusbar/drawstatusbar/MainActivity.java
@@ -2,13 +2,36 @@ package draw.statusbar.drawstatusbar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    Button btnChangeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        frameLayout = (FrameLayout) findViewById(R.id.fl_container);
+        btnChangeFragment = (Button) findViewById(R.id.btn_change);
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.fl_container, FirstFragment.newInstance())
+                .commit();
+
+        btnChangeFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fl_container, SecondFragment.newInstance())
+                        .commit();
+            }
+        });
+
     }
+
 }
--- app/src/main/java/draw/statusbar/drawstatusbar/SecondFragment.java
+++ app/src/main/java/draw/statusbar/drawstatusbar/SecondFragment.java
@@ -1,11 +1,13 @@
 package draw.statusbar.drawstatusbar;
 
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 /**
  * Created by css on 3/23/17.
@@ -26,6 +28,11 @@ public class SecondFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getActivity().getWindow()
+                    .clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
 
         return view;
     }
